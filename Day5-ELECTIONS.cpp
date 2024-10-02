9975f685a6c24004107349a512d70bbfa2740dac
Mohit Singh07 <76769125+mohitsingh0709@users.noreply.github.com>
2024-10-02 14:52:59 +0530

    Add files via upload

---
+++ Day5-ELECTIONS.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x1,x2,x3;
+	    cin>>x1>>x2>>x3;
+	    if(x1>50){
+	        cout<<"A"<<endl;
+	    }
+	    else if(x2>50){
+	        cout<<"B"<<endl;
+	    }
+	    else if(x3>50){
+	        cout<<"C"<<endl;
+	    }
+	    else
+	    cout<<"nota"<<endl;
+	    
+	}
+
+}
