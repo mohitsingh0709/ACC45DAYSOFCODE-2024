d76748439370d9e01b0f97c67a827555eb9e2f56
Mohit Singh07 <76769125+mohitsingh0709@users.noreply.github.com>
2024-10-04 10:43:32 +0530

    Add files via upload

---
+++ Day7-JASSIGNMENTS.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x>7)
+	    cout<<"no"<<endl;
+	    else
+	    cout<<"yes"<<endl;
+	    
+	    
+	}
+
+}
