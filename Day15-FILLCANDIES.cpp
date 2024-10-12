898585263d4876c0cc6cdeca70c8f8912de965c6
Mohit Singh07 <76769125+mohitsingh0709@users.noreply.github.com>
2024-10-12 14:11:44 +0530

    Add files via upload

---
+++ Day15-FILLCANDIES.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int a,b,c,d;
+	    cin >> a>> b>> c;
+	    d=b*c;
+	    if(a%d ==0)
+	    cout << a/d << endl;
+	    else
+	    cout << a/d + 1 << endl;
+	}
+	return 0;
+}
