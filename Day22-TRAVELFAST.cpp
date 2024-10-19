947d0b047ab297707b9669e42c121d49e534302b
Mohit Singh07 <76769125+mohitsingh0709@users.noreply.github.com>
2024-10-19 15:25:36 +0530

    Add files via upload

---
+++ Day22-TRAVELFAST.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if(x==y){
+	    cout<<"same"<<endl;
+	    }
+	    else if(x<y){
+	    cout<<"bike"<<endl;
+	    }
+	    else{
+	    cout<<"car"<<endl;
+	    }
+	    
+	}
+
+}
