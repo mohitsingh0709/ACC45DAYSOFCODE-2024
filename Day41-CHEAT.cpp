b41ee7ca534373294a4aaba66464a6943f1c7bb7
Mohit Singh07 <76769125+mohitsingh0709@users.noreply.github.com>
2024-11-07 11:45:10 +0530

    Add files via upload

---
+++ Day41-CHEAT.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n<2)
+        cout<<"0"<<endl;
+        else
+        {
+            if (n%7 >= 2)
+            cout<< (n/7) + 1 <<endl;
+            else
+            cout<< n/7 <<endl;
+        }
+    }
+    return 0;
+}
