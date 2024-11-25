9a3f3d41a97ad8369e6c334acffb90955866500a
Mohit Singh07 <76769125+mohitsingh0709@users.noreply.github.com>
2024-11-25 23:19:02 +0530

    Add files via upload

---
+++ day57-CUTOFF.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    int arr[n];
+	    for(int i=0;i<n;i++){
+	        cin>>arr[i];
+	    }
+	    sort(arr,arr+n);
+	    cout<<arr[n-x]-1<<endl;
+	}
+	return 0;
+}
